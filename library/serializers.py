1c9219b9351272cf00fbdb6af7d87f5657ca3167
Oleksandr Mykhailyk <stekloduv45@gmail.com>
2024-12-05 12:54:27 +0100

    add serializers.py in library and update admin.py

---
+++ library/serializers.py
@@ -0,0 +1,22 @@
+from rest_framework import serializers
+
+from library.models import Book, Borrowing
+
+
+class BookSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Book
+        fields = ("id", "title", "author", "cover", "inventory", "daily_fee")
+
+
+class BorrowingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Borrowing
+        fields = (
+            "id",
+            "borrow_date",
+            "expected_return_date",
+            "actual_return_date",
+            "book",
+            "user",
+        )
