9705bac4240db974bc2ed996f7e1db9dcd898fc2
Oleksandr Mykhailyk <stekloduv45@gmail.com>
2024-12-05 12:29:39 +0100

    update user models.py add serializers.py in user models.py, update settings.py

--- library/models.py
+++ library/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+from user.models import User
+
+
+class Book(models.Model):
+    class CoverType(models.TextChoices):
+        HARD = "HARD", "Hardcover"
+        SOFT = "SOFT", "Softcover"
+
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    cover = models.CharField(max_length=4, choices=CoverType.choices)
+    inventory = models.PositiveIntegerField()
+    daily_fee = models.DecimalField(max_digits=6, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.title} by {self.author}"
+
+
+class Borrowing(models.Model):
+    borrow_date = models.DateField(verbose_name="Borrow Date")
+    expected_return_date = models.DateField(verbose_name="Expected Return Date")
+    actual_return_date = models.DateField(
+        verbose_name="Actual Return Date", null=True, blank=True
+    )
+    book = models.ForeignKey("Book", on_delete=models.CASCADE, verbose_name="Book ID")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="User ID")
+
+    def __str__(self):
+        return f"Borrowing: {self.book.title} by {self.user.username}"
