be1dde2f5983b69afbdfeca4a492258ef615b94d
Oleksandr Mykhailyk <stekloduv45@gmail.com>
2024-12-06 11:54:02 +0100

    update

--- book/admin.py
+++ book/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 
-from book.models import Book, Borrowing
+from book.models import Book
 
 admin.site.register(Book)
-admin.site.register(Borrowing)
--- borrowing/urls.py
+++ borrowing/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, include
 from rest_framework import routers
 
 from book.views import BookViewSet
+from borrowing.views import BorrowingViewSet
 
 router = routers.DefaultRouter()
 router.register("borrowing", BorrowingViewSet)
@@ -10,4 +11,4 @@ urlpatterns = [
     path("", include(router.urls)),
 ]
 
-app_name = "book"
+app_name = "borrowing"
